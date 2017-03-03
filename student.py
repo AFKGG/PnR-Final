68155272f53dce1b2f99a7a4d726e77c25bbd3e4
zhuj <juntaozhu233@gmail.com>
2017-03-03 10:43:34 -0500

    Count Obstacles

--- student.py
+++ student.py
@@ -60,6 +60,35 @@ class GoPiggy(pigo.Pigo):
         # activate the item selected
         menu.get(ans, [None, error])[1]()
 
+    def count_obstacle(self):
+        self.wide_scan()
+        # count how many obstacles I've found
+        counter = 0
+        found_something = False
+        # loop through all my scan data
+        for x in self.scan:
+            # if x is not None and really close
+            if x and x <= self.STOP_DIST:
+                # if I've already found something
+                if found_something:
+                    print("obstacle continues")
+                # if this is a new obstacles
+                else:
+                    # switch my tracker
+                    found_something = True
+                    print("start of new obstacle")
+            # if my data show safe distances...
+            if x and x > self.STOP_DIST:
+                # if my tracker is been triggered
+                if found_something:
+                    print("end of obstacle")
+                    # reset tracker
+                    found_something = False
+                    # increase count of obstacles
+                    counter += 1
+        print('Total number of obstacles in this scan: ' + str(counter))
+        return counter
+
     def turn_test(self):
         while True:
             ans = raw_input('Turn right, left or stop? (r/l/s): ')
@@ -76,9 +105,9 @@ class GoPiggy(pigo.Pigo):
     def restore_heading(self):
         print("Now I will turn back to the starting position.")
         if self.turn_track > 0:
-            self.encL(abs(self.turn_track) * 1.5)
+            self.encL(abs(self.turn_track))
         elif self.turn_track < 0:
-            self.encR(abs(self.turn_track) * 1.5)
+            self.encR(abs(self.turn_track))
 
 
 
