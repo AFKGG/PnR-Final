eae2b029f141b173efde1cecc22db7c652a076a8
zhuj <juntaozhu233@gmail.com>
2017-03-15 10:53:49 -0400

    Going around the obstacle

--- student.py
+++ student.py
@@ -202,16 +202,17 @@ class GoPiggy(pigo.Pigo):
                 self.encF(10)
                 count += 1
                 if count >= 2 and self.turn_track != 0:
+                    # Moving forward before restore heading which creates distance from the previous obstacle
                     self.encF(3)
                     self.restore_heading()
                     count = 0
             answer = self.choose_path()
             if answer == "left":
                 self.encL(5)
-                self.encF(10)
+                self.encF(2)
             elif answer == "right":
                 self.encR(5)
-                self.encF(10)
+                self.encF(2)
 
     def encR(self, enc):
         pigo.Pigo.encR(self, enc)
