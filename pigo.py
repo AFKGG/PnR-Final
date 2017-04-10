fd0b2f9b47bed26c8e63400b0b8c522ccd928732
zhuj <juntaozhu233@gmail.com>
2017-04-10 11:47:20 -0400

    Increase the area of scanning

--- pigo.py
+++ pigo.py
@@ -157,7 +157,7 @@ class Pigo(object):
 
     def is_clear(self):
         print("Running the is_clear method.")
-        for x in range((self.MIDPOINT - 15), (self.MIDPOINT + 15), 5):
+        for x in range((self.MIDPOINT - 25), (self.MIDPOINT + 25), 5):
             servo(x)
             time.sleep(.1)
             scan1 = us_dist(15)
