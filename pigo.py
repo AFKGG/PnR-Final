5dd89ae99ae3866bc3571649ed70daaf0cba31b0
zhuj <juntaozhu233@gmail.com>
2017-04-21 11:23:01 -0400

    Refine restore heading

--- pigo.py
+++ pigo.py
@@ -160,14 +160,14 @@ class Pigo(object):
         for x in range((self.MIDPOINT - 25), (self.MIDPOINT + 25), 6):
             servo(x)
             time.sleep(.1)
-            scan1 = us_dist(15)
+            scan1 = us_dist(20)
             time.sleep(.1)
             # double check the distance
-            scan2 = us_dist(15)
+            scan2 = us_dist(20)
             time.sleep(.1)
             # if I found a different distance the second time....
             if abs(scan1 - scan2) > 2:
-                scan3 = us_dist(15)
+                scan3 = us_dist(20)
                 time.sleep(.1)
                 # take another scan and average the three together
                 scan1 = (scan1 + scan2 + scan3) / 3
