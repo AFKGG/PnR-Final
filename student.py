a206ffd7cd2d352716263bc73b573a84ce5e34b4
zhuj <juntaozhu233@gmail.com>
2017-04-26 11:29:00 -0400

    Fix both wheels speed

--- student.py
+++ student.py
@@ -18,7 +18,7 @@ class GoPiggy(pigo.Pigo):
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 85
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
-        self.STOP_DIST = 28
+        self.STOP_DIST = 25
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.LEFT_SPEED = 135
         # YOU DECIDE: What left motor power helps straighten your fwd()?
