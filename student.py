c4d7dcc6b11252d825fdc84ca056a70574671cc4
zhuj <juntaozhu233@gmail.com>
2017-05-10 11:16:46 -0400

    Logging.debug

--- student.py
+++ student.py
@@ -243,7 +243,6 @@ class GoPiggy(pigo.Pigo):
                 if self.dist() > self.STOP_DIST + 20:
                     self.encF(5)
                     self.restore_heading()
-                    logging.info("Restore heading")
                     return
                 self.servo(self.MIDPOINT)
         # Check left side
@@ -255,7 +254,6 @@ class GoPiggy(pigo.Pigo):
                 if self.dist() > self.STOP_DIST + 20:
                     self.encF(5)
                     self.restore_heading()
-                    logging.info("Restore heading")
                     return
                 self.servo(self.MIDPOINT)
 
